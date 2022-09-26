0f6dcf25eeedc603bcc9761afdc3c53ac8e3aa56
ashna-24 <ashnafrajan@gmail.com>
2022-09-26 20:54:19 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,144 +1,205 @@
 function validateform(){
-    var uname=document.forms['myform']['uname'];
-    var address=document.forms['myform']['address'];
-    var email=document.forms['myform']['email'];
-    var password=document.forms['myform']['password'];
-    var number=document.forms['myform']['number'];
-    var telephone=document.forms['myform']['telephone'];
-    var month=document.forms['myform']['month'];
-    var week=document.forms['myform']['week'];
-    var datetime=document.forms['myform']['datetime'];
-    var localdatetime=document.forms['myform']['localdatetime'];
-    var uploadfile=document.forms['myform']['uploadfile'];
+   var uname=document.forms['myform']['uname'];
+   var gender=document.forms['myform']['gender'];
+   var address=document.forms['myform']['address'];
+   var age=document.forms['myform']['age'];
+   var email=document.forms['myform']['email'];
+   var password=document.forms['myform']['password'];
+   var number=document.forms['myform']['number'];
+   var telephone=document.forms['myform']['telephone'];
+   var qualify=document.forms['myform']['study'];
+   var datetime=document.forms['myform']['datetime'];
+   var proof=document.forms['myform']['proof'];
+   var uploadfile=document.forms['myform']['uploadfile'];
 
-    if(uname.value==""){
-      document.getElementById('name_div').style.visibility="visible";
-      uname.style.border="1px solid red";  
-      return false;
-    }
-    else if(uname.value.length<3){
-       document.getElementById('name_div').style.visibility="visible";
-       uname.style.border="1px solid red";
-       return false;
-    }else{
-       uname.style.border="1px solid #5e6e66";
-       name_div.innerHTML="";
-       return true;
-    }
-    
-    if(address.value==""){
-      document.getElementById('address_div').style.visibility="visible";
-      address.style.border="1px solid red";  
-      return false;
-    }
-    else if(address.value.length<8){
-       document.getElementById('address_div').style.visibility="visible";
-       address.style.border="1px solid red";
-       return false;
-    }else{
-       address.style.border="1px solid #5e6e66";
-       adress_div.innerHTML="";
-       return true;
-    }
+   nameValidate(uname);
+   genderValidate(gender);
+   addressValidate(address);
+   ageValidate(age);
+   emailValidate(email);
+   passwordValidate(password);
+   numberValidate(number);
+   telephoneValidate(telephone);
+   qualifyValidate(qualify);
+   datetimeValidate(datetime);
+   proofValidate(proof);
+   fileValidate(uploadfile);
+   return false;
+}
 
-    if(email.value==""){
-      document.getElementById('email_div').style.visibility="visible";
-      email.style.border="1px solid red";  
-      return false;
-    }else{
-       email.style.border="1px solid #5e6e66";
-       email_div.innerHTML="";
-       return true;
-    }
+function errorValidate(){
+  document.getElementById('name_div').style.visibility="visible";
+  document.getElementById('gender_div').style.visibility="visible";
+  document.getElementById('address_div').style.visibility="visible";
+  document.getElementById('age_div').style.visibility="visible";
+  document.getElementById('email_div').style.visibility="visible";
+  document.getElementById('pswd_div').style.visibility="visible";
+  document.getElementById('number_div').style.visibility="visible"
+  document.getElementById('datetime_div').style.visibility="visible";
+  document.getElementById('tel_div').style.visibility="visible";
+  document.getElementById('qualify_div').style.visibility="visible";
+  document.getElementById('proof_div').style.visibility="visible"; 
+  document.getElementById('uploadfile_div').style.visibility="visible";
+}
 
-    if(password.value==""){
-      document.getElementById('pswd_div').style.visibility="visible";
-      password.style.border="1px solid red";  
+function nameValidate(uname){
+   
+   if(uname.value==""){
+     errorValidate();
+     uname.style.border="1px solid red";  
+     return false;
+   }
+   else if(uname.value.length<3){
+      errorValidate();
+      uname.style.border="1px solid red"; 
       return false;
-    }else{
-       password.style.border="1px solid #5e6e66";
-       pswd_div.innerHTML="";
-       return true;
-    }
+   }
+   else{
+      document.getElementById("name_div").innerHTML="";
+      uname.style.border="1px solid green";
+      return true;
+   }
+}
 
-    if(number.value==""){
-      document.getElementById('number_div').style.visibility="visible";
-      number.style.border="1px solid red";  
-      return false;
-    }
-    else if(number.value.length<=10){
-       document.getElementById('number_div').style.visibility="visible";
-       number.style.border="1px solid red";
-       return false;
-    }else{
-       number.style.border="1px solid #5e6e66";
-       number_div.innerHTML="";
-       return true;
-    }
+function genderValidate(gender){
+   if(gender.value==""){
+     errorValidate();  
+     return false;
+   }else{
+      document.getElementById("gender_div").innerHTML="";
+      return true;
+   }
+}
 
-    if(telephone.value==""){
-      document.getElementById('tel_div').style.visibility="visible";
-      telephone.style.border="1px solid red";  
+function addressValidate(address){    
+   if(address.value==""){
+     errorValidate();
+     address.style.border="1px solid red";  
+     return false;
+   }
+   else if(address.value.length<8){
+      errorValidate();
+      address.style.border="1px solid red"; 
       return false;
-    }
-    else if(telephone.value.length<=10){
-       document.getElementById('tel_div').style.visibility="visible";
-       telephone.style.border="1px solid red";
-       return false;
-    }else{
-       telephone.style.border="1px solid #5e6e66";
-       tel_div.innerHTML="";
-       return true;
-    }
-    
-    if(month.value==""){
-      document.getElementById('month_div').style.visibility="visible";
-      month.style.border="1px solid red";  
-      return false;
-    }else{
-       month.style.border="1px solid #5e6e66";
-       month_div.innerHTML="";
-       return true;
-    }
-    
-    if(week.value==""){
-      document.getElementById('week_div').style.visibility="visible";
-      week.style.border="1px solid red";  
-      return false;
-    }else{
-       week.style.border="1px solid #5e6e66";
-       week_div.innerHTML="";
-       return true;
-    }
+   }
+   else{
+      document.getElementById("address_div").innerHTML="";
+      address.style.border="1px solid green";
+      return true;
+   }
+}
 
-    if(datetime.value==""){
-      document.getElementById('datetime_div').style.visibility="visible";
-      datetime.style.border="1px solid red";  
-      return false;
-    }else{
-       datetime.style.border="1px solid #5e6e66";
-       datetime_div.innerHTML="";
-       return true;
-    }
+function ageValidate(age){
+   if(age.value==""){
+      errorValidate();
+     return false;
+   }else{
+      document.getElementById("age_div").innerHTML="";
+      return true;
+   }
+}
 
-    if(localdatetime.value==""){
-      document.getElementById('local_div').style.visibility="visible";
-      localdatetime.style.border="1px solid red";  
-      return false;
-    }else{
-       localdatetime.style.border="1px solid #5e6e66";
-       local_div.innerHTML="";
-       return true;
-    }
+function emailValidate(email){
+   if(email.value==""){
+     errorValidate();
+     email.style.border="1px solid red";  
+     return false;
+   }else{
+      email.style.border="1px solid green";
+      email_div.innerHTML="";
+      return true;
+   }
+}
 
-    
-    if(uploadfile.value==""){
-      document.getElementById('uploadfile_div').style.visibility="visible";
-      uploadfile.style.border="1px solid red";  
+function passwordValidate(password){
+   if(password.value==""){
+    errorValidate(); 
+     password.style.border="1px solid red";  
+     return false;
+   }else{
+      password.style.border="1px solid green";
+      pswd_div.innerHTML="";
+      return true;
+   }
+}
+
+function numberValidate(number){
+   if(number.value==""){
+     errorValidate();
+     number.style.border="1px solid red";  
+     return false;
+   }/*
+   else if(number.value.length<=10){
+      errorValidate();
+      number.style.border="1px solid red"; 
       return false;
-    }else{
-       uploadfile.style.border="1px solid #5e6e66";
-       uploadfile_div.innerHTML="";
-       return true;
-    }
+   }*/else{
+      number.style.border="1px solid green";
+      number_div.innerHTML="";
+      return true;
+   }
+}
+
+function telephoneValidate(telephone){
+   if(telephone.value==""){
+     errorValidate();
+     telephone.style.border="1px solid red";  
+     return false;
+   }/*
+   else if(telephone.value.length<=10){
+     errorValidate();
+      telephone.style.border="1px solid red"; 
+      return false;
+   }*/else{
+      telephone.style.border="1px solid green";
+      tel_div.innerHTML="";
+      return true;
+   }
+}
+
+function qualifyValidate(qualify){     
+   if(qualify.value==""){
+     errorValidate();
+     qualify.style.border="1px solid red";  
+     return false;
+   }else{
+      qualify.style.border="1px solid green";
+      qualify_div.innerHTML="";
+      return true;
+   }
+}
+
+function datetimeValidate(datetime){
+   if(datetime.value==""){
+     errorValidate();
+     datetime.style.border="1px solid red";  
+     return false;
+   }else{
+      datetime.style.border="1px solid green";
+      datetime_div.innerHTML="";
+      return true;
+   }
+}
+
+function proofValidate(proof){
+   if(proof.value==""){
+     errorValidate();
+     return false;
+   }else{
+      document.getElementById("proof_div").innerHTML="";
+      return true;
+   }
+}
+
+
+function fileValidate(uploadfile){     
+   if(uploadfile.value==""){
+     errorValidate();
+     uploadfile.style.border="1px solid red";  
+     return false;
+   }else{
+      uploadfile.style.border="1px solid green";
+      uploadfile_div.innerHTML="";
+      return true;
+   }
 }
